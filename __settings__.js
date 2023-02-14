1c9c7f246e224edea18af9267201cdc4ed6a8ef3
gsbroz <gs@gsbroz.com>
2023-02-14 13:11:15 -0500

    initial world upload

---
+++ __settings__.js
@@ -0,0 +1,23 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "1657068.json";
+CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': true,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+SCRIPTS = [ 120713230, 120713238, 120713240, 120713254, 120713257, 120713264 ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: true,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/120713223/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/120713224/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/120713222/1/ammo.js', 'preload' : true},
+    {'moduleName' : 'BASIS', 'glueUrl' : 'files/assets/120713227/1/basis.wasm.js', 'wasmUrl' : 'files/assets/120713228/1/basis.wasm.wasm', 'fallbackUrl' : 'files/assets/120713226/1/basis.js', 'preload' : false},
+];
