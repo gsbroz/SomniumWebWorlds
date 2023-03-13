dfad96ddbf5c57800f1055b73b35a28042ae2f66
gsbroz <gs@gsbroz.com>
2023-03-13 02:51:58 -0400

    v10
    disabled local player simulator

--- __settings__.js
+++ __settings__.js
@@ -1,23 +1,23 @@
-ASSET_PREFIX = "";
-SCRIPT_PREFIX = "";
-SCENE_PATH = "1657068.json";
-CONTEXT_OPTIONS = {
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "1657068.json";
+window.CONTEXT_OPTIONS = {
     'antialias': true,
     'alpha': true,
     'preserveDrawingBuffer': false,
     'preferWebGl2': true,
     'powerPreference': "default"
 };
-SCRIPTS = [ 120713230, 120713238, 120713240, 120713254, 120713257, 120713264 ];
-CONFIG_FILENAME = "config.json";
-INPUT_SETTINGS = {
+window.SCRIPTS = [ 120713230, 120713238, 120713240, 120713254, 120713257, 120713264 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
     useKeyboard: true,
     useMouse: true,
     useGamepads: true,
     useTouch: true
 };
 pc.script.legacy = false;
-PRELOAD_MODULES = [
+window.PRELOAD_MODULES = [
     {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/120713223/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/120713224/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/120713222/1/ammo.js', 'preload' : true},
     {'moduleName' : 'BASIS', 'glueUrl' : 'files/assets/120713227/1/basis.wasm.js', 'wasmUrl' : 'files/assets/120713228/1/basis.wasm.wasm', 'fallbackUrl' : 'files/assets/120713226/1/basis.js', 'preload' : false},
 ];
